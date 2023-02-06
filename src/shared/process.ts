02d924844dee05859215ecf724ecb05bb251bd59
Olyno <olyno.dev@gmail.com>
2023-02-07 00:11:25 +0100

    fix: build issues

--- src/shared/process.ts
+++ src/shared/process.ts
@@ -3,7 +3,11 @@ import { spawn } from 'node:child_process';
 export async function execute(command: string) {
   return new Promise((resolve, reject) => {
     const args = command.split(' ');
-    const child = spawn(args[0], args.slice(1), { stdio: 'inherit' });
+    const child = spawn(args[0], args.slice(1), {
+      cwd: process.cwd(),
+      stdio: 'inherit',
+      shell: true,
+    });
     child.addListener('close', resolve);
     child.addListener('exit', resolve);
     child.addListener('error', reject);
